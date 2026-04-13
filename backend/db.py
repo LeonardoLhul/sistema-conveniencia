fd7076565f9f5a839f66e1239c0b0ca7233f0763
Leonardo Lhul <129051238+LeonardoLhul@users.noreply.github.com>
2026-04-13 19:18:55 -0300

    Update database connection credentials

--- backend/db.py
+++ backend/db.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",          # usuário do MySQL
-        password="*1407*", # senha do MySQL
+        host="",
+        user="senhadocliente",          # usuário do MySQL
+        password="senhadocliente", # senha do MySQL
         database="conveniencia"
     )
